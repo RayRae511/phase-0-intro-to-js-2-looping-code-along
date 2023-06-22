71592950b995000634c9057c70d9eb5142e6e0f4
Raymond <oetaray@gmail.com>
2023-06-22 20:20:54 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function writeCards(names,event){
+    let messages = [
+        "Thank you, dad, for the wonderful gift!"
+    ];
+
+    for(let i=0; i < names.length; i++){
+    let message = `Thanks, ${names[i]}, for the wonderful ${event} gift!`
+    messages = [...messages,message];
+    }
+
+    return messages;
+}
+
+
+function countDown(number = 10){
+    while(number >= 0){
+        number--;
+        console.log(number);
+    }
+}
